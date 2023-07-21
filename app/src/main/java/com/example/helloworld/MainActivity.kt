a690470565c7a9572b519715c010592093763004
yeonuk44 <yeonuk44@gmail.com>
2023-07-21 14:22:38 +0900

    # Add code structure info

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,4 +1,6 @@
+// 무조건 포함되는 package 파일. 계층 구조를 나타냅니다.
 package com.example.helloworld
+
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,8 +14,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.helloworld.ui.theme.HelloWorldTheme
 
-class MainActivity : ComponentActivity() {
+// MainActivity는 ComponentActivity 클래스를 상속받고 있다.
+class MainActivity : ComponentActivity() { // MainActivity -> ComponentActivity -> Activity
+    // MainActivity의 생명주기가 실행되는 가장 첫 번째 콜백함수이다.
     override fun onCreate(savedInstanceState: Bundle?) {
+        /**
+         * Info
+         * onCreate() 함수에는 savedInstanceState라는 매개변수가 존재한다.
+         * 이는 특정한 상황에서 Activity가 종료될 때 Bundle에 값을 저장하기 위함이다.
+         * 특정한 상황의 예: 화면 방향 변경, 메모리 부족으로 인한 앱 종료 등
+         * Bundle 타입은 여러 데이터 타입의 박스 같은 느낌이라고 생각하면 된다. 여러 데이터가 들어오면 맵핑을 한다.
+         * 별 다른 세팅을 해주지 않으면 savedInstanceState의 값이 Null로 리턴된다.
+         */
         super.onCreate(savedInstanceState)
         setContent {
             HelloWorldTheme {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("yeonuk")
                 }
             }
         }
